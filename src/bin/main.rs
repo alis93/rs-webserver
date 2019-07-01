cf16b864c7092ebbf2d496acbab098078fea69b5
sully <sully.s.a.786@googlemail.com>
2019-07-01 17:20:17 +0100

    completed server

---
+++ src/bin/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use std::thread;
+use std::time::Duration;
+
+use rs_webserver::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    thread::sleep(Duration::from_millis(4000)); //artificial delay to show how single threaded server would be slow if multiple connections
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 200 OK\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+// This is used to allow a function to take ownership of a boxed value.
+// According to docs, this won't be needed in future (HOPEFULLY!)
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F> FnBox for F
+where
+    F: FnOnce(),
+{
+    fn call_box(self: Box<F>) {
+        (*self)();
+    }
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    queue: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero or less.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        let (tx, rx) = mpsc::channel();
+
+        let rx = Arc::new(Mutex::new(rx));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, rx.clone()));
+        }
+        ThreadPool { workers, queue: tx }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Message::NewJob(Box::new(f));
+        &self.queue.send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // Send termination message to workers.
+        // two loops required since they wont receive message in order.
+        for _ in &mut self.workers {
+            self.queue.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                println!("Shutting down worker {}", worker.id);
+
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                // get reference to mutex. lock it. receive job.
+                let message: Message = receiver.as_ref().lock().unwrap().recv().unwrap();
+                match message {
+                    Message::NewJob(j) => {
+                        println!("Worker {} got a job; executing.", id);
+                        j.call_box()
+                    }
+                    Message::Terminate => {
+                        println!("Worker {} terminating.", id);
+                        break;
+                    }
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
